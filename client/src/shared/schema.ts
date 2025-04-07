d0eaf4bdb562fea8107de55d15b8f2d0ed477981
John Prada <jhonprada@gmail.com>
2025-04-07 18:04:03 -0500

    chore: configure tailwind and fix client dependencies

---
+++ client/src/shared/schema.ts
@@ -0,0 +1,19 @@
+export const fabricTypes = [
+  { id: 'aida14', name: 'Aida 14 count', count: 14 },
+  { id: 'aida16', name: 'Aida 16 count', count: 16 },
+  { id: 'aida18', name: 'Aida 18 count', count: 18 },
+  { id: 'evenweave28', name: 'Evenweave 28 count', count: 28 },
+  { id: 'evenweave32', name: 'Evenweave 32 count', count: 32 },
+  { id: 'linen28', name: 'Linen 28 count', count: 28 },
+  { id: 'linen32', name: 'Linen 32 count', count: 32 },
+  { id: 'linen36', name: 'Linen 36 count', count: 36 },
+  { id: 'linen40', name: 'Linen 40 count', count: 40 }
+];
+
+export const threadTypes = [
+  { id: 'dmc', name: 'DMC', brand: 'DMC' },
+  { id: 'anchor', name: 'Anchor', brand: 'Anchor' },
+  { id: 'sullivans', name: 'Sullivans', brand: 'Sullivans' },
+  { id: 'cosmo', name: 'Cosmo', brand: 'Cosmo' },
+  { id: 'jpcotton', name: 'J&P Coats', brand: 'J&P Coats' }
+];
